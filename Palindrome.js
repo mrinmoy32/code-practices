0a84a23bb2759a214f6cb43765fe32972eef27a4
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-09 20:09:59 +0530

    Better approach

--- Palindrome.js
+++ Palindrome.js
@@ -4,19 +4,41 @@
 
 //********Check Palindrome********
 
-let givenWord = "racecar";
-let ourStack = [];  // We are using this empty Array as a stack to push the letters
-let reverseWord = "";
+// let givenWord = "racecar";
+// let ourStack = [];  // We are using this empty Array as a stack to push the letters
+// let reverseWord = "";
 
-for(i=0; i<givenWord.length; i++){
-    ourStack.push(givenWord[i]);
-}
-console.log(ourStack);
+// for(i=0; i<givenWord.length; i++){
+//     ourStack.push(givenWord[i]);
+// }
+// console.log(ourStack);
 
-for(i=0; i<givenWord.length; i++) {
-    reverseWord += ourStack.pop();
-}
-console.log("reverseWord =" , reverseWord);
+// for(i=0; i<givenWord.length; i++) {
+//     reverseWord += ourStack.pop();
+// }
+// console.log("reverseWord =" , reverseWord);
 
-if(givenWord === reverseWord) console.log(givenWord , "is a Plaindrome");
-else console.log(givenWord , "is not a Plaindrome");
+// if(givenWord === reverseWord) console.log(givenWord , "is a Plaindrome");
+// else console.log(givenWord , "is not a Plaindrome");
+
+//Simpler way in js =================================>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+const readline = require("readline");
+
+const reverseString = string => string.split("").reverse().join("");
+
+const checkPalindrome = string =>
+  reverseString(string) === string
+    ? console.log(string, "is a palindrome")
+    : console.log(string, "is not a palindrome");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter a string: ", (input) => {
+  // console.log(input);
+  checkPalindrome(input);
+  rl.close();
+});
