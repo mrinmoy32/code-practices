e5dc7ecdacff632a2a775568f262ed0c870446cc
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-13 21:56:20 +0530

    Bubble Sort with user input data

--- bubbleSort.js
+++ bubbleSort.js
@@ -4,14 +4,23 @@ const swap = (array, i, j) => {
     array[j] = temp;
   };
 
-const bubbleSort = (array) => {
+const commandLineStringToArray = (string) => string.split(" ");
+
+const bubbleSort = (Str) => {
+    arrayStr = commandLineStringToArray(Str);
+    array = arrayStr.map(item => parseInt(item));
     for(let i=0; i<array.length-1; i++){
         for(let j=0; j<array.length-i-1; j++){
            if (array[j]>array[j+1]) swap(array, j, j+1);
         }
     }
+    console.log(array);
     return array;
 }
 
-const list = [100, 5, 3, 1, 2, 0]; //sample array
-console.log(bubbleSort(list)); //calling bubbleSort to sort sample array
+// const list = [100, 5, 3, 1, 2, 0]; //sample array
+// console.log(bubbleSort(list)); //calling bubbleSort to sort sample array
+
+console.log("please enter the data to sort with single space") //To take input as e.g. 12 1 0 11 11 100 5
+const takeInput = require('./readline');
+takeInput(bubbleSort);
